9ed9175d64a70d95bb6a44e1e9b2367d8b42eaad
Administrator <droidrui@126.com>
2017-07-29 17:36:13 +0800

    Initial commit

---
+++ src/main/java/com/mxt/dsb/DsbApplication.java
@@ -0,0 +1,12 @@
+package com.mxt.dsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsbApplication.class, args);
+	}
+}
